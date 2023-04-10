991d454fa27fbc5bb5a581ef738cec5e4660edf7
ViniciusAndBar <vini.zeus2002@gmail.com>
2023-04-10 08:54:43 -0300

    Cadastro Pet

---
+++ src/screens/CadastroPet/index.tsx
@@ -0,0 +1,31 @@
+import { useNavigation } from '@react-navigation/native'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { CadastroPetstyle } from './styles'
+import { TextInputMask } from 'react-native-masked-text';
+
+export const CadastroPet = () => {
+    const navigation = useNavigation ()
+    return (
+        <View style={CadastroPetstyle.View}>
+            <Text style={CadastroPetstyle.Text}>Cadastrar Pet</Text>
+            <Text style={CadastroPetstyle.Label}>Nome</Text>
+            <TextInput style={CadastroPetstyle.TextInput}></TextInput>
+            <Text style={CadastroPetstyle.Label}>Animal</Text>
+            <TextInput style={CadastroPetstyle.TextInput}></TextInput>
+            <Text style={CadastroPetstyle.Label}>Raça</Text>
+            <TextInput style={CadastroPetstyle.TextInput}></TextInput>
+            <Text style={CadastroPetstyle.Label}>Tipo</Text>
+            <TextInput style={CadastroPetstyle.TextInput}></TextInput>
+            <Text style={CadastroPetstyle.Label}>Data Nascimento</Text>
+            <TextInputMask 
+            type={'datetime'}
+            options={{
+                format: 'YYYY/MM/DD'
+              }}
+            style={CadastroPetstyle.TextInput}></TextInputMask>
+            <Text style={CadastroPetstyle.Label}>Genero</Text>
+            <TextInput style={CadastroPetstyle.TextInput}></TextInput>
+            <TouchableOpacity onPress={() => navigation.navigate("Home")}><Text style={CadastroPetstyle.Textbutton}>Cadastrar</Text></TouchableOpacity>
+        </View>
+    )
+}
---
+++ src/screens/CadastroPet/styles.ts
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native'
+
+export const CadastroPetstyle = StyleSheet.create({
+    View: {
+        display: "flex",
+        backgroundColor: "#76BDFF",
+        height: "100%",
+        width:"100%",
+        alignSelf: "center",
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    Text: {
+        alignSelf: "center",
+        fontSize: 50,
+        color: "#2C3877",
+    },
+    Label: {
+        fontSize: 18,
+        color: "#2C3877",
+        marginTop: 15,
+    },
+    TextInput: {
+        width: 288,
+        height: 35,
+        backgroundColor: "#FFFFFF",
+        borderRadius: 8,
+        borderColor: 'gray', 
+        borderWidth: 1,
+    },
+    Textbutton: {
+        backgroundColor:"#257CFF",
+        color:"#fff",
+        width: 200,
+        height: 35,
+        borderRadius: 8,
+        borderColor: 'gray', 
+        borderWidth: 1,
+        fontSize: 18,
+        textAlign: 'center',
+        padding: 3,
+        marginTop: 15
+    }
+})
