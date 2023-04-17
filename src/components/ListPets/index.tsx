1d63952751dd89fab230d9990d9185898b4e65be
ViniciusAndBar <vini.zeus2002@gmail.com>
2023-04-17 08:21:45 -0300

    Inicio da componentização - listPet e listServico

---
+++ src/components/ListPets/index.tsx
@@ -0,0 +1,25 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { ListPetstyle } from './styles'
+
+export const ListPets = () => {
+
+
+
+  return (
+    <View>
+      <Image
+        source={{}}
+        style={{}}
+      />
+      <View>
+        <Text>
+            Nome do pet
+        </Text>
+      </View>
+      <View>
+        <TouchableOpacity>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
