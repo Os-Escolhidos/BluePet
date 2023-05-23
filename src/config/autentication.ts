f6393a2b67567be1500c2fd4cbde7e6f3f296a2f
KevinFMfatec <mirendakevin@gmail.com>
2023-05-23 08:36:16 -0300

    Imagem dos pets e associação do pet ao usuário

---
+++ src/config/autentication.ts
@@ -0,0 +1,22 @@
+import React from "react";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth/react-native";
+
+const auth = getAuth();
+
+export function useAuthentication() {
+  const [user, setUser] = React.useState<User>();
+  React.useEffect(() => {
+    const unsubscribeFromAuthStatuChanged = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(undefined);
+      }
+    });
+
+    return unsubscribeFromAuthStatuChanged;
+  }, []);
+  return {
+    user,
+  };
+}
--- src/screens/CadastroPet/index.tsx
+++ src/screens/CadastroPet/index.tsx
@@ -1,14 +1,35 @@
 import { useNavigation } from '@react-navigation/native'
 import { CadastroPetstyle } from './styles'
 import "../../config/firebase"
-import React, { useState } from 'react';
-import { addDoc, collection } from 'firebase/firestore';
-import { db } from '../../config/firebase';
+import React, { useEffect, useState } from 'react';
+import { addDoc, arrayUnion, collection, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
+import { db, storage } from '../../config/firebase';
 import { Layout, Text, Input, Button } from '@ui-kitten/components/ui';
+import { uuidv4 } from "@firebase/util";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { launchImageLibraryAsync, MediaTypeOptions } from "expo-image-picker";
+import { useAuthentication } from '../../config/autentication';
+
+interface ISendFiles {
+  mimeType: string;
+  name: string;
+  size: number;
+  type: string;
+  uri: string;
+}
+
+interface IFiles {
+  id: string;
+  url: string;
+  type: string;
+}
 
 export const CadastroPet = () => {
+  const { user } = useAuthentication()
   const navigation = useNavigation()
   const [errorMessage, setErrorMessage] = useState("");
+  const [imageSelect, setImageSelect] = useState<ISendFiles>();
+  const [loading, setLoading] = useState(false);
 
   const [value, setValue] = useState({
     nome: "",
@@ -24,19 +45,55 @@ export const CadastroPet = () => {
       return;
     }
     try {
-      await addDoc(collection(db, "pets"), {
-        nome: value.nome,
-        animal: value.animal,
-        raca: value.raca,
-        tipo: value.tipo,
-        idade: value.idade,
-        sexo: value.sexo
+      const FotoPet = await UploadSingleImage(imageSelect)
+      const refDoc = doc(db, `usuarios`, user.uid)
+      await updateDoc(refDoc, {
+        pets: arrayUnion({
+          nome: value.nome,
+          animal: value.animal,
+          raca: value.raca,
+          tipo: value.tipo,
+          idade: value.idade,
+          sexo: value.sexo,
+          img: {
+            id: FotoPet.id,
+            url: FotoPet.url
+          }
+        })
       }).then(() => navigation.navigate("Home"));
     }
     catch (error: any) {
       setErrorMessage(error);
     }
   }
+
+  const pickImage = async () => {
+    let result: any = await launchImageLibraryAsync({
+      mediaTypes: MediaTypeOptions.Images,
+      videoQuality: 1,
+      allowsMultipleSelection: false,
+      quality: 1,
+    });
+    if (result.canceled) {
+      return;
+    } else {
+      setImageSelect(result.assets[0]);
+      setLoading(true);
+    }
+  };
+
+  const UploadSingleImage = async (image: ISendFiles) => {
+    const id = uuidv4();
+    const response = await fetch(image.uri);
+    const blob = await response.blob();
+    const imageRef = ref(storage, `images/${id}`);
+    const uploadStatus = uploadBytesResumable(imageRef, blob);
+    const snapshot = await uploadStatus;
+    return {
+      id,
+      url: await getDownloadURL(snapshot.ref),
+    };
+  };
 
   return (
     <Layout style={CadastroPetstyle.View}>
@@ -61,6 +118,8 @@ export const CadastroPet = () => {
 
       <Text category='h6' style={CadastroPetstyle.Label}>Sexo</Text>
       <Input style={CadastroPetstyle.TextInput} onChangeText={(text) => setValue({ ...value, sexo: text })}></Input>
+
+      <Button size='large' onPress={pickImage} style={CadastroPetstyle.Button}>ADICIONAR IMAGEM</Button>
 
       <Button size='large' onPress={handleRegister} style={CadastroPetstyle.Button}>CADASTRAR</Button>
     </Layout>
