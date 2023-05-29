779e8999803a265f382a4793b99ab83fa5a775fe
KevinFMfatec <mirendakevin@gmail.com>
2023-05-29 08:27:09 -0300

    Inicio da Listagem de Pets

--- src/screens/ListagemPet/index.tsx
+++ src/screens/ListagemPet/index.tsx
@@ -3,46 +3,64 @@ import { ListPets } from "../../components/ListPets";
 import { ListPetstyle } from './styles'
 import { Card, Layout, Text } from "@ui-kitten/components/ui";
 import React, { useCallback, useEffect, useState } from "react";
-import { collection, getDocs, query } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../config/firebase";
+import { useAuthentication } from "../../config/autentication";
 
-interface pets {
-  nome: string
+interface Ipets {
+  body: {
+    animal: String;
+    idade: String;
+    nome: String;
+    raca: String;
+    tipo: String;
+    sexo: String;
+    img: {
+      url: String;
+    }
+  }[]
 }
 
 const ListSearchedPets = () => {
-  /* const route = useRoute<ListPets>(); */
 
-const [ Pets , setPets ] = useState <pets[]>([])
+  const [Pets, setPets] = useState<Ipets[]>([])
+  const { user } = useAuthentication()
 
   const findAllPets = useCallback(
     async () => {
       let petsData: any[] = [];
-      const collect = collection(db, "pets");
+      const collect = collection(db, `usuarios`);
       const queryFilterDate = query(
-        collect,
+        collect, where("id", "==", String(user?.uid))
       );
       const querySnapshot = await getDocs(queryFilterDate);
       querySnapshot.forEach((doc) => {
         petsData.push({
           id: doc.id,
-          body: doc.data(),
+          body: doc.data().pets,
         });
       });
       setPets(petsData);
     },
-    [setPets]
+    [setPets, user]
   );
 
-  useEffect (() => {
+  useEffect(() => {
     findAllPets()
-  })
+  }, [user?.uid])
 
-  console.log(Pets)
-
+  console.log(Pets[0].body)
   return (
     <Layout style={ListPetstyle.View}>
-      
+        {
+          Pets[0].body.map(i => {
+            return(
+              <Card>
+                <Text>{i.animal}</Text>
+              </Card>
+            )
+          })
+        }
     </Layout>
   );
 };
